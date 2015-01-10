7e3e16bc0afec5e59067c0239f4f26b05f6d9a5c
itzik <itzik@king>
2015-01-10 17:49:29 +0200

    Signed-off-by: itzik <itzik@king>

--- src/view/View.java
+++ src/view/View.java
@@ -6,8 +6,5 @@ import model.*;
 	public abstract void start();//start the view
 	public abstract void displayCurrentState();//show me now what happens in game
 	public abstract void displaySolution(Solution solution);//show show solution
-	//itzik
-	//itzik
-	//itzik
 }
 	
